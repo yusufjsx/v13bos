73cd0ed9d7b68beaa9ab3b6125a7c4d09d5ea4e2
YusufDev <97362501+yusufjsx@users.noreply.github.com>
2023-09-30 21:42:05 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
         client.user.setPresence({
             activities: [
                 {
-                    name: `Github.com YusufJS`,
+                    name: `Github.com yusufjsx`,
                     type: "PLAYING"
                 }
             ],
